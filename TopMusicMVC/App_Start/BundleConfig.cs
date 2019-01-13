3a53695a0d6dd26e987f5d911a1a2ba79d98d378
Leroy Bastien <Falnestu@hotmail.com>
2019-01-13 11:39:21 +0100

    Init MVC Project with progressus template

---
+++ TopMusicMVC/App_Start/BundleConfig.cs
@@ -0,0 +1,40 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace TopMusicMVC
+{
+    public class BundleConfig
+    {
+        // Pour plus d'informations sur le regroupement, visitez https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Utilisez la version de développement de Modernizr pour le développement et l'apprentissage. Puis, une fois
+            // prêt pour la production, utilisez l'outil de génération à l'adresse https://modernizr.com pour sélectionner uniquement les tests dont vous avez besoin.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/headroom").Include(
+                      "~/Scripts/headroom.min.js",
+                      "~/Scripts/jQuery.headroom.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/template").Include(
+                      "~/Scripts/template.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/bootstrap-theme.css",
+                      "~/Content/main.css",
+                      "~/Content/Site.css"));
+        }
+    }
+}
